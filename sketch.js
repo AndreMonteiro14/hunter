9f948408b3b2a5441ebf8e123dbbb9279a58f293
m823 <accm1407@gmail.com>
2017-03-06 10:53:02 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,155 +1,80 @@
-var hunters = [];
-var d = -0.25;
-var difficulty = 0.008;
-var currentR = 5.5;
-var growth = 0.7;
-var respawn = true;
-var numCircles = 40;
-var decoys = true;
-var decoyOn = true;
-var decoyLife = 200;
-var currentDecoy = decoyLife;
-var decoyCooldown = 1000;
-var currentCooldown = decoyCooldown;
-var allowDecoy = true;
-var huntX;
-var huntY;
-var decoyX;
-var decoyY;
-var goFinal = false;
-var showNums = false;
-var endless = false;
-var newPos1;
-var newPos2;
-var newR;
+var scl = 20;
+var snake;
+var foods = [];
+var powerup = [];
+var powerup2 = [];
+var fr = 10;
+var pm = 1;
 
 function setup() {
-    createCanvas(windowWidth-10,windowHeight-10); 
-    for(var i = 0; i < numCircles; i++){
-        hunters.push(new Hunter(random(20,width-20),random(20,height-20),random(currentR-2,currentR*5),random(20,220),random(20,220),random(20,220)));
-    }
+    createCanvas(windowWidth,windowHeight);
+    snake = new Snake();  
+    foods[0] = round(random(1,width/scl-1));
+    foods[1] = round(random(1,height/scl-1));
+    powerup[0] = round(random(1,width/scl-1));
+    powerup[1] = round(random(1,height/scl-1));
+    powerup2[0] = round(random(1,width/scl-1));
+    powerup2[1] = round(random(1,height/scl-1));
+    frameRate(fr);
 }
 
 function draw() {
-    background(0);
-    
-    if(decoys === false){
-        huntX = mouseX;
-        huntY = mouseY;
-    } else {
-        if(decoyOn === false || allowDecoy === false || goFinal === false){
-            huntX = mouseX;
-            huntY = mouseY;
-        } else {
-            huntX = decoyX;
-            huntY = decoyY;
-            fill(255);
-            ellipse(decoyX,decoyY,20,20);
-            currentDecoy--;
+    background(255);
+    strokeWeight(3)
+    noFill();
+    rect(2,2,width-2,height-2);
+    strokeWeight(1)
+    snake.display();
+    snake.update();
+    keyPressed = function(){
+        if(keyCode === UP_ARROW){
+            snake.dir = [0,-1];
+        } else if(keyCode === DOWN_ARROW){
+            snake.dir = [0,1];
+        } else if(keyCode === LEFT_ARROW){
+            snake.dir = [-1,0];
+        } else if(keyCode === RIGHT_ARROW){
+            snake.dir = [1,0];
         }
     }
-    
-    if(mouseX > width-currentR){
-        mouseX = width-currentR;
-    } 
-    
-    if(mouseY > height-currentR){
-        mouseY = height-currentR;
+    fill(30,200,30);
+    rect(foods[0]*scl,foods[1]*scl,scl,scl);
+    if(snake.x === foods[0] && snake.y === foods[1]){
+        snake.length += (2 * pm);
+        foods[0] = round(random(1,width/scl-1));
+        foods[1] = round(random(1,height/scl-1));
     }
     
-    if(mouseX < currentR){
-        mouseX = currentR;    
+    fill(20,20,255)
+    rect(powerup[0]*scl,powerup[1]*scl,scl,scl);
+    if(snake.x === powerup[0] && snake.y === powerup[1]){
+        fr *= 1.1;
+        frameRate(fr);
+        powerup[0] = round(random(1,width/scl-1));
+        powerup[1] = round(random(1,height/scl-1));
+    }
+
+    fill(255,20,255);
+    rect(powerup2[0]*scl,powerup2[1]*scl,scl,scl);
+    if(snake.x === powerup2[0] && snake.y === powerup2[1]){
+        pm *= 1.05;
+        powerup2[0] = round(random(1,width/scl-1));
+        powerup2[1] = round(random(1,height/scl-1));
     }
     
-    if(mouseY < currentR){
-        mouseY = currentR;
+    if(snake.x > round(width/scl)-1 || snake.x < 0 || snake.y < 0 || snake.y > round(height/scl)-1){
+        noLoop();
+    }
+    
+    for(var i = snake.history.length-1; i >= snake.history.length-snake.length; i--){
+        if(snake.history[i][0] === snake.x && snake.history[i][1] === snake.y){
+            noLoop();
+        }
     }
     textSize(15);
-    fill(255);
-    text("Decoy Cooldown: " + round(currentCooldown),width-160,height-30);
-    text("Current Size: " + round(currentR),width-160,height-10);
-    text("Andre Monteiro",10,height-30);
-    text("PD: 3",10,height-10);
-    if(endless === true){
-        text("Endless",width/2-10,height-10);
-    }
-    
-    for(var i = hunters.length-1; i > -1; i--){
-        hunters[i].update();
-        hunters[i].display();
-        var fe = p5.Vector.sub(hunters[i].position,createVector(huntX,huntY));
-        if(hunters[i].r < currentR){
-            fe.mult(-d);    
-        } else {
-            fe.mult(d);
-        }
-        hunters[i].applyForce(fe);
-        if(hunters[i].position.x < hunters[i].r*2 || hunters[i].position.x > width - hunters[i].r*2){
-            hunters[i].velocity.x *= -0.8;
-        }
-        
-        if(hunters[i].position.y < hunters[i].r*2 || hunters[i].position.y > height - hunters[i].r*2){
-            hunters[i].velocity.y *= -0.8;
-        }
-        
-        if(dist(hunters[i].position.x,hunters[i].position.y,mouseX,mouseY) < (hunters[i].r+currentR)){
-            if(currentR > hunters[i].r){
-                currentR += hunters[i].r/TWO_PI * growth;
-                hunters.splice(i,1);
-                var go = false;
-                while(go === false){
-                    newPos1 = random(20,width-20);
-                    newPos2 = random(20,height-20);
-                    if(endless === true){
-                        newR  = random(currentR*0.5,currentR*2);
-                    } else {
-                        newR = random(3,27.5);
-                    }
-                    if(dist(mouseX,mouseY,newPos1,newPos2) > 1.1*(currentR + newR)){
-                        go = true;
-                    }
-                }
-                hunters.push(new Hunter(newPos1,newPos2,newR,random(20,255),random(20,255),random(20,255))); 
-            } else {
-                noLoop();
-            }
-        }
-    }
-    
-    keyPressed = function(){
-        if(key == ' '){
-            goFinal = true;
-            decoyX = mouseX;
-            decoyY = mouseY;
-        }    
-    }
-    
-    keyTyped = function(){
-        if(key == 'w'){
-            if(showNums === false){
-                showNums = true;
-            } else {
-                showNums = false;
-            }
-        } else if(key == 's'){
-            endless = true;
-        }
-    }
-    
-    if(currentDecoy < 1){
-        allowDecoy = false;
-        goFinal = false;
-    }
-    
-    if(allowDecoy === false){
-        currentCooldown--;
-        if(currentCooldown < 1){
-            allowDecoy = true;
-            currentCooldown = decoyCooldown;
-            currentDecoy = decoyLife;
-        }
-    }
-    fill(255);
-    ellipse(mouseX,mouseY,currentR*2,currentR*2)
-    d -= difficulty; 
+    fill(0);
+    text("Speed: " + round(fr),width-80,height-20);
+    text("Multiplier: " + pm, width-100,height-40)
+    text("Andre Monteiro",20,height-40)
+    text("Period 3",20,height-20)
 }
