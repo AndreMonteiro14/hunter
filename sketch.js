5d84714f14e7c50c3ca5ccaf0feed8c5d41f28a2
m823 <accm1407@gmail.com>
2017-06-30 21:25:12 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,13 @@ var currentFrame = 0;
 var thickness = 1;
 var playMode = false;
 var layers = [];
+var fr = 20;
+var eraserMode = false;
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
 	frs.push(new Frame());
-	//layers.push(new Layer(frs[0]));
+	layers.push(new Layer(frs[0]));
 	for(var i = 50; i < 160; i++){
 		frameRate(i);
 	}
@@ -19,7 +21,7 @@ function draw() {
 	if(playMode === true){
 		frs[currentFrame].display(0);
 		currentFrame += 1;
-		if(currentFrame === frs.length-1){
+		if(currentFrame >= frs.length-1){
 			currentFrame = 0;
 		}
 	} else {
@@ -46,11 +48,27 @@ function draw() {
 			if(key == "p"){
 				currentFrame = 0;
 				playMode = true;
-				frameRate(15);
+				frameRate(fr);
+			} else if(key == "="){
+				//fr += 5;
+				thickness += 1;
+			} else if(key == "-"){
+				//fr -= 5;
+				thickness -= 1;
+			} else if(key == "e"){
+				if(eraserMode === false){
+					eraserMode = true;
+				} else {
+					eraserMode = false;
+				}
 			}
 		}
 	}
-	textSize(15);
 	fill(0);
+	textSize(12);
+	text("Frame Rate: " + round(frameRate()),width-100,20);
+	text("Frame " + (currentFrame+1) + "/" + frs.length,width-100,40);
+	text("Thickness: " + thickness,width-100,60);
+	textSize(15);
 	text("By Andre Monteiro",width-150,height-20);
 }
