5ee04f9ee554a3e43aa7ee00b1c2437ca0708bd1
m823 <accm1407@gmail.com>
2017-07-02 20:23:00 -0400

    Update hunter.js

--- hunter.js
+++ hunter.js
@@ -1,35 +1,24 @@
 var Frame = function(){
-	this.canvas = [];
-	for(var i = 0; i < width*height; i++){
-		this.canvas.push(0);
-	}
+	this.marks = [];
 };
 
 Frame.prototype.edit = function(x,y){
-	// if(eraserMode === false){
-	// 	this.canvas[ (x*height) + y] = 1;
-	// } else {
-	// 	this.canvas[ (x*height) + y] = 0;
-	// }
-	for(var i = -1*(thickness-1); i <= (thickness-1); i++){
-		for(var j = -1*(thickness-1); j <= (thickness-1); j++){
-			if(eraserMode === false){
-				this.canvas[ ((x+i)*height) + (y+j)] = 1;
-			} else {
-				this.canvas[ ((x+i)*height) + (y+j)] = 0;
+	if(eraserMode === false){
+		this.marks.push(new Array(x,y,thickness));
+	} else {
+		for(var k = 0; k < this.marks.length; k++){
+			if(dist(x,y,this.marks[k][0],this.marks[k][1]) < thickness){
+				this.marks.splice(k,1);
 			}
 		}
 	}
 }
 
 Frame.prototype.display = function(opacity){
+	loadPixels();
 	fill(255*opacity);
 	stroke(255*opacity);
-	for(var x = 0; x < width; x++){
-		for(var y = 0; y < height; y++){
-			if(this.canvas[ (x*height) + y] === 1){
-				ellipse(x,y,1,1);
-			}
-		}
+	for(var i = 0; i < this.marks.length; i++){
+		ellipse(this.marks[i][0],this.marks[i][1],this.marks[i][2],this.marks[i][2]);
 	}
 }
