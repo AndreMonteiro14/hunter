ce05b02f173337d84f37fb4d3e3c706b46a863c0
m823 <accm1407@gmail.com>
2017-07-02 20:22:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,18 @@ var thickness = 1;
 var playMode = false;
 var layers = [];
 var eraserMode = false;
+var drawing = false;
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
 	frs.push(new Frame());
-	//layers.push(new Layer(frs[0]));
+	layers.push(new Layer(frs[0]));
 }
 
 function draw() {
 	background(255);
+	noFill();
+	ellipse(mouseX,mouseY,thickness,thickness);
 	if(playMode === true){
 		frs[currentFrame].display(0);
 		currentFrame += 1;
@@ -22,11 +25,11 @@ function draw() {
 		}
 	} else {
 		if(currentFrame !== 0){
-			frs[currentFrame-1].display(0.9);
+			frs[currentFrame-1].display(0.75);
 		}
 		frs[currentFrame].display(0);
 		mouseDragged = function(){
-		  frs[currentFrame].edit(mouseX,mouseY)
+		  frs[currentFrame].edit(mouseX,mouseY);
 		}
 		
 		keyPressed = function(){
@@ -42,14 +45,12 @@ function draw() {
 		
 		keyTyped = function(){
 			if(key == "p"){
-				frameRate(20);
+				frameRate(15);
 				currentFrame = 0;
 				playMode = true;
 			} else if(key == "="){
-				//fr += 5;
 				thickness += 1;
 			} else if(key == "-"){
-				//fr -= 5;
 				thickness -= 1;
 			} else if(key == "e"){
 				if(eraserMode === false){
@@ -62,9 +63,14 @@ function draw() {
 	}
 	fill(0);
 	textSize(12);
-	//text("Frame Rate: " + round(frameRate()),width-100,20);
+	text("Frame Rate: " + round(frameRate()),width-100,20);
 	text("Frame " + (currentFrame+1) + "/" + frs.length,width-100,40);
 	text("Thickness: " + thickness,width-100,60);
+	if(eraserMode === true){
+		text("Eraser Mode On",width-100,80);
+	} else {
+		text("Eraser Mode Off",width-100,80);
+	}
 	textSize(15);
 	text("By Andre Monteiro",width-150,height-20);
 }
